62150d089c0371c30ef6c3cfd7148ed864fa5b5e
unsubstantiated-Script <jusfulton@gmail.com>
2026-03-25 14:30:43 -0500

    Server main file complete

---
+++ server/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	pb "gRPCCoffee/gen"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+type server struct {
+	pb.UnimplementedCoffeeShopServer
+}
+
+func (s *server) GetMenu(ctx context.Context, req *pb.MenuRequest) (*pb.Menu, error) {
+	return &pb.Menu{
+		Items: []*pb.Item{
+			{Id: "1", Name: "Black Coffee"},
+			{Id: "2", Name: "Americano"},
+			{Id: "3", Name: "Vanilla Soy Chai Latte"},
+		},
+	}, nil
+}
+
+func (s *server) PlaceOrder(ctx context.Context, order *pb.Order) (*pb.Receipt, error) {
+	return &pb.Receipt{Id: "ABC123"}, nil
+}
+
+func (s *server) GeOrderStatus(ctx context.Context, receipt *pb.Receipt) (*pb.OrderStatus, error) {
+	return &pb.OrderStatus{
+		OrderId: receipt.Id,
+		Status:  "In Progress",
+	}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":9001")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterCoffeeShopServer(grpcServer, &server{})
+
+	log.Println("Server started")
+	log.Fatal(grpcServer.Serve(lis))
+}
